7a6d63fdcfca53b183a38dbff885228d1fb6dd14
AnusreeUthaman <anusreeuthaman02@gmail.com>
2024-08-16 09:27:48 +0530

    pythonday3

---
+++ day03.py
@@ -0,0 +1,92 @@
+'''
+a=10
+b=5
+c=10
+print(id(a))
+print(id(b))
+print(id(c))
+'''
+'''
+a=5
+b=10
+c=5
+
+print(id(a))
+print(id(b))
+print(id(c))
+'''
+'''
+print(a is b)
+print(a is c)
+'''
+'''
+'''
+a="codeme"
+'''print("code" in "codeme")
+'''
+'''
+a="codeme"
+print("demo" not in "codeme")
+'''
+
+'''
+x=3.14159
+print(round(X))
+print(round(x,2))
+print(round(x,3))
+'''
+'''
+#length
+text="welcome to me"
+length=len(text)
+print(length)
+
+text="anusree"
+length=len(text)
+print(length)
+
+#input()
+name=input("Enter your name:")
+print("my name is "+name)
+
+
+
+'''
+'''name=input("enter your name:")
+age=input("enter your age:")
+print("my name is "  +name+ " and i'm "+age+" years old")
+'''
+
+#string formating
+#format method
+
+name=input("enter your name :")
+age=int(input("enter your age :"))
+print("my name is"+name+ " and i'm "+age+"years old")
+'''
+name=input("enter your name : ")
+age=int(input("enter your age : "))
+print("my name is" +name " and i'm " +age+" years old")
+print("my name is {} and i'm {} years old".format(name,age))
+'''
+'''
+name=input("enter your name:")
+age=int(
+print(f"my name is {name} and i'm {age} years old")
+'''
+'''
+#conditional statement
+#if
+x=int(input("enter a number: "))
+if x>10:
+    print(f"{x} is greater than 10")
+print("End")
+
+#if- else
+x=int(input("enter a number: "))
+if x%2 == 0:
+    print(f"{x} is even")
+else:
+    print(f"{x} is odd")
+print("program end")
+'''
